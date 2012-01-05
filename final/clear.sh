9627cfdb0a14f48819c8d098695c285e047d9e96
Karata Brown <vabc3h@gmail.com>
2012-01-05 17:25:52 +0800

    New clear script

--- final/clear.sh
+++ final/clear.sh
@@ -1,3 +1,5 @@
-rm -rf Makefile.in
-rm -rf auto* *scan
 find -name '*~' -exec rm {} +
+find -name 'auto*' -exec rm -rf {} +
+find -name '*scan' -exec rm {} +
+find -name '*in' -exec rm {} +
+find -name '.deps' -exec rm -rf {} +
